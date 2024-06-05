e0fecc86f5c5d27df6e6006969d86f8b254037b5
1davidyu1 <yudavid@msu.edu>
2024-06-05 18:33:09 -0500

    centering and max width

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,18 +1,30 @@
-"use client"
+"use client";
 
+import { GeistSans } from 'geist/font/sans';
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { Twitter, Linkedin } from "lucide-react";
+import Link from "next/link";
 
 export const Navbar = () => {
     const scrolled = useScrollTop();
     return (
-        <div className={cn("z-50 bg-background fixed top-16 left-0 right-0 flex items-center justify-between p-6", scrolled && "border-b shadow-sm")}>
-            <h1 className="pl-16 text-2xl font-bold">David</h1>
-            <div className="flex items-center gap-x-2 pr-16">
-                <Twitter className="text-muted-foreground h-5 w-5"/>
-                <Linkedin className="text-muted-foreground h-5 w-5"/>
+        <div className='flex justify-center'>
+            <div className={cn("z-50 fixed top-16 flex items-center justify-between max-w-xl w-full", scrolled && "border-b shadow-sm")}>
+                <Link href="/">
+                    <h1 className="text-2xl font-bold">
+                        David Yu
+                    </h1>
+                </Link>
+                <div className="flex items-center gap-x-4">
+                    <Link href="https://x.com/1davidyu1">
+                        <Twitter className="text-muted-foreground h-6 w-6"/>
+                    </Link>
+                    <Link href="https://www.linkedin.com/in/david-yu-871202244/">
+                        <Linkedin className="text-muted-foreground h-6 w-6"/>
+                    </Link>
+                </div>
             </div>
         </div>
-    )
-}
+    );
+};
